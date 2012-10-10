51fa2dbf99c4efa3c0dca8539b0e9c31eb046796
Jacek <jacek@kuzemczak.co.uk>
2012-10-10 23:54:44 +0100

    Initial commit.

---
+++ src/main/java/uk/co/jacekk/bukkit/simpleirc/SimpleIRC.java
@@ -0,0 +1,13 @@
+package uk.co.jacekk.bukkit.simpleirc;
+
+import uk.co.jacekk.bukkit.baseplugin.v3.BasePlugin;
+
+public class SimpleIRC extends BasePlugin {
+	
+	public void onEnable(){
+		super.onEnable(true);
+		
+		
+	}
+	
+}
