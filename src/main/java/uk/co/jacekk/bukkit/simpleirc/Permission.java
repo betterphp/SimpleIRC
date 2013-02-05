d43fe1e3cdb5b938309fdd939bc058f249132ad2
Jacek <jacek@kuzemczak.co.uk>
2013-02-05 07:29:30 +0000

    Make use of the new sub-command system, also add more fine-grained permissions.

--- src/main/java/uk/co/jacekk/bukkit/simpleirc/Permission.java
+++ src/main/java/uk/co/jacekk/bukkit/simpleirc/Permission.java
@@ -6,8 +6,12 @@ import uk.co.jacekk.bukkit.baseplugin.v9.permissions.PluginPermission;
 
 public class Permission {
 	
-	public static final PluginPermission GIVE_OP		= new PluginPermission("simpleirc.op.give", PermissionDefault.OP, "Allowes the player to give OP to other players");
-	public static final PluginPermission TAKE_OP		= new PluginPermission("simpleirc.op.take", PermissionDefault.OP, "Allowes the player to take OP from other players");
-	public static final PluginPermission MANAGE_CHANNEL	= new PluginPermission("simpleirc.channel.manage", PermissionDefault.OP, "Allowes the player to manage the IRC channel using the /irc command");
+	public static final PluginPermission GIVE_OP	= new PluginPermission("simpleirc.op.give", PermissionDefault.OP, "Allowes the player to give OP to other players");
+	public static final PluginPermission TAKE_OP	= new PluginPermission("simpleirc.op.take", PermissionDefault.OP, "Allowes the player to take OP from other players");
+	
+	public static final PluginPermission KICK		= new PluginPermission("simpleirc.channel.kick", PermissionDefault.OP, "Allowes the player to kick IRC users");
+	public static final PluginPermission BAN		= new PluginPermission("simpleirc.channel.ban", PermissionDefault.OP, "Allowes the player to ban IRC users");
+	public static final PluginPermission OP			= new PluginPermission("simpleirc.channel.op", PermissionDefault.OP, "Allowes the player to op IRC users");
+	public static final PluginPermission VOICE		= new PluginPermission("simpleirc.channel.voice", PermissionDefault.OP, "Allowes the player to deop IRC users");
 	
 }
--- src/main/java/uk/co/jacekk/bukkit/simpleirc/command/IRCCommandExecutor.java
+++ src/main/java/uk/co/jacekk/bukkit/simpleirc/command/IRCCommandExecutor.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 import uk.co.jacekk.bukkit.baseplugin.v9.command.BaseCommandExecutor;
 import uk.co.jacekk.bukkit.baseplugin.v9.command.CommandHandler;
 import uk.co.jacekk.bukkit.baseplugin.v9.command.CommandTabCompletion;
+import uk.co.jacekk.bukkit.baseplugin.v9.command.SubCommandHandler;
 import uk.co.jacekk.bukkit.simpleirc.Permission;
 import uk.co.jacekk.bukkit.simpleirc.SimpleIRC;
 
@@ -18,83 +19,138 @@ public class IRCCommandExecutor extends BaseCommandExecutor<SimpleIRC> {
 	@CommandHandler(names = {"irc"}, description = "Allowes a player to manage the IRC channel", usage = "[action] <args>")
 	@CommandTabCompletion({"kick|ban|op|deop|voice|devoice"})
 	public void ircop(CommandSender sender, String label, String[] args){
-		if (!Permission.MANAGE_CHANNEL.has(sender)){
+		sender.sendMessage(ChatColor.RED + "Usage: /" + label + " <action> <args>");
+		sender.sendMessage(ChatColor.RED + "Actions:");
+		
+		if (Permission.KICK.has(sender)){
+			sender.sendMessage(ChatColor.RED + "  kick - Kicks a user from a channel");
+		}
+		
+		if (Permission.BAN.has(sender)){
+			sender.sendMessage(ChatColor.RED + "  ban - Bans a hostmask from a channel");
+		}
+		
+		if (Permission.OP.has(sender)){
+			sender.sendMessage(ChatColor.RED + "  op - Gives a user OP");
+			sender.sendMessage(ChatColor.RED + "  deop - Takes OP from a user");
+		}
+		
+		if (Permission.VOICE.has(sender)){
+			sender.sendMessage(ChatColor.RED + "  voice - Gives voice to a user");
+			sender.sendMessage(ChatColor.RED + "  devoice - Takes voice from a user");
+		}
+	}
+	
+	@SubCommandHandler(parent = "irc", name = "kick")
+	public void ircKick(CommandSender sender, String label, String[] args){
+		if (!Permission.KICK.has(sender)){
 			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
 			return;
 		}
 		
-		if (args.length == 0){
-			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " <action> <args>");
-			sender.sendMessage(ChatColor.RED + "Actions:");
-			sender.sendMessage(ChatColor.RED + "  kick - Kicks a user from a channel");
-			sender.sendMessage(ChatColor.RED + "  ban - Bans a hostmask from a channel");
-			sender.sendMessage(ChatColor.RED + "  op - Gives a user OP");
-			sender.sendMessage(ChatColor.RED + "  deop - Takes OP from a user");
-			sender.sendMessage(ChatColor.RED + "  voice - Gives voice to a user");
-			sender.sendMessage(ChatColor.RED + "  devoice - Takes voice from a user");
+		if (args.length < 2){
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " kick <channel> <nick> <reason>");
 			return;
 		}
 		
-		String action = args[0].toLowerCase();
-		
-		if (action.equals("kick")){
-			if (args.length < 3){
-				sender.sendMessage(ChatColor.RED + "Usage: /" + label + " kick <channel> <nick> <reason>");
-				return;
+		if (args.length > 2){
+			String reason = args[2];
+			
+			for (int i = 3; i < args.length; ++i){
+				reason += " " + args[i];
 			}
 			
-			if (args.length > 3){
-				String reason = args[3];
-				
-				for (int i = 4; i < args.length; ++i){
-					reason += " " + args[i];
-				}
-				
-				plugin.bot.kick(args[1], args[2], reason);
-			}else{
-				plugin.bot.kick(args[1], args[2]);
-			}
-		}else if (action.equals("ban")){
-			if (args.length < 3){
-				sender.sendMessage(ChatColor.RED + "Usage: /" + label + " ban <channel> <hostmask>");
-				return;
-			}
-			
-			plugin.bot.ban(args[1], args[2]);
-		}else if (action.equals("op")){
-			if (args.length < 3){
-				sender.sendMessage(ChatColor.RED + "Usage: /" + label + " op <channel> <nick>");
-				return;
-			}
-			
-			plugin.bot.op(args[1], args[2]);
-		}else if (action.equals("deop")){
-			if (args.length < 3){
-				sender.sendMessage(ChatColor.RED + "Usage: /" + label + " deop <channel> <nick>");
-				return;
-			}
-			
-			plugin.bot.deOp(args[1], args[2]);
-		}else if (action.equals("voice")){
-			if (args.length < 3){
-				sender.sendMessage(ChatColor.RED + "Usage: /" + label + " voice <channel> <nick>");
-				return;
-			}
-			
-			plugin.bot.voice(args[1], args[2]);
-		}else if (action.equals("devoice")){
-			if (args.length < 3){
-				sender.sendMessage(ChatColor.RED + "Usage: /" + label + " devoice <channel> <nick>");
-				return;
-			}
-			
-			plugin.bot.deVoice(args[1], args[2]);
+			plugin.bot.kick(args[0], args[1], reason);
 		}else{
-			sender.sendMessage(ChatColor.RED + "Invalid action, try /" + label + " for a list of actions");
+			plugin.bot.kick(args[0], args[1]);
+		}
+		
+		sender.sendMessage(ChatColor.GREEN + args[1] + " has been kicked from " + args[0]);
+	}
+	
+	@SubCommandHandler(parent = "irc", name = "ban")
+	public void ircBan(CommandSender sender, String label, String[] args){
+		if (!Permission.BAN.has(sender)){
+			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
 			return;
 		}
 		
-		sender.sendMessage(ChatColor.GREEN + "IRC command sent");
+		if (args.length != 2){
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " ban <channel> <hostmask>");
+			return;
+		}
+		
+		plugin.bot.ban(args[0], args[1]);
+		
+		sender.sendMessage(ChatColor.GREEN + args[1] + " has been banned from " + args[0]);
+	}
+	
+	@SubCommandHandler(parent = "irc", name = "op")
+	public void ircOp(CommandSender sender, String label, String[] args){
+		if (!Permission.OP.has(sender)){
+			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+			return;
+		}
+		
+		if (args.length < 3){
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " op <channel> <nick>");
+			return;
+		}
+		
+		plugin.bot.op(args[0], args[1]);
+		
+		sender.sendMessage(ChatColor.GREEN + args[1] + " has been oped in " + args[0]);
+	}
+	
+	@SubCommandHandler(parent = "irc", name = "deop")
+	public void ircDeop(CommandSender sender, String label, String[] args){
+		if (!Permission.OP.has(sender)){
+			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+			return;
+		}
+		
+		if (args.length < 3){
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " deop <channel> <nick>");
+			return;
+		}
+		
+		plugin.bot.deOp(args[0], args[1]);
+		
+		sender.sendMessage(ChatColor.GREEN + args[1] + " has been deoped in " + args[0]);
+	}
+	
+	@SubCommandHandler(parent = "irc", name = "voice")
+	public void ircVoice(CommandSender sender, String label, String[] args){
+		if (!Permission.VOICE.has(sender)){
+			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+			return;
+		}
+		
+		if (args.length != 2){
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " voice <channel> <nick>");
+			return;
+		}
+		
+		plugin.bot.voice(args[0], args[1]);
+		
+		sender.sendMessage(ChatColor.GREEN + args[1] + " has been given voice in " + args[0]);
+	}
+	
+	@SubCommandHandler(parent = "irc", name = "devoice")
+	public void ircDevoice(CommandSender sender, String label, String[] args){
+		if (!Permission.VOICE.has(sender)){
+			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+			return;
+		}
+		
+		if (args.length != 2){
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " devoice <channel> <nick>");
+			return;
+		}
+		
+		plugin.bot.deVoice(args[0], args[1]);
+		
+		sender.sendMessage(ChatColor.GREEN + args[1] + " no longer has voice in " + args[0]);
 	}
 	
 }
